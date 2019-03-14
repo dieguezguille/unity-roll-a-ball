b379021f4dc12ab9f64a60d377ccdff40734873b
Guillermo Agustín Diéguez <guillermodieguez@outlook.com>
2019-03-14 02:08:22 -0300

    First commit
    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 _compensacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _compensacion = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + _compensacion;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed;
+    public Rigidbody Rb;
+    private int _points;
+    public Text CountText;
+    public Text WinText;
+
+    void Start()
+    {
+        Rb.GetComponent<Rigidbody>();
+        _points = 0;
+        setPointsText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // var speed = Vector3.forward * Speed;
+
+        Rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * Speed, ForceMode.Force);       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            _points += 1;
+            setPointsText();
+        }
+    }
+
+    void setPointsText()
+    {
+        CountText.text = "Points: " + _points;
+        if (_points == 5)
+        {
+            WinText.text = "YOU WON!";
+        }
+    }
+}
